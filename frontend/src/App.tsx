555afad6fd9fd63433006337ad88c4b6034ad874
MarceloSilvaRosa <marcelosrosa83@gmail.com>
2022-01-11 23:29:19 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 function App() {
   return (
-    <h1>Oágina principal</h1>
+    <h1>Página principal</h1>
   );
 }
 
